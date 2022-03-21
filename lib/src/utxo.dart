e0b5a4acfcb752dc6cdae21774dac25239bec7f4
ethicnology <ethicnology@pm.me>
2022-03-21 11:41:37 +0100

    fix: utxo deserialization check values

--- lib/src/utxo.dart
+++ lib/src/utxo.dart
@@ -2,10 +2,10 @@ import 'status.dart';
 import 'utils.dart';
 
 class Utxo {
-  String txid;
-  int vout;
-  int value;
-  Status status;
+  late String txid;
+  late int vout;
+  late int value;
+  late Status status;
 
   Utxo(this.txid, this.vout, this.value, this.status) {
     isEqual(txid.length, 64);
@@ -14,11 +14,16 @@ class Utxo {
     isPositive(vout);
   }
 
-  Utxo.fromJson(Map<String, dynamic> json)
-      : txid = json['txid'],
-        vout = json['vout'],
-        value = json['value'],
-        status = Status.fromJson(json['status']);
+  Utxo.fromJson(Map<String, dynamic> json) {
+    txid = json['txid'];
+    vout = json['vout'];
+    value = json['value'];
+    status = Status.fromJson(json['status']);
+    isEqual(txid.length, 64);
+    isPositive(vout);
+    isPositive(value);
+    isPositive(vout);
+  }
 
   Map<String, dynamic> toJson() =>
       {'txid': txid, 'vout': vout, 'value': value, 'status': status.toJson()};
