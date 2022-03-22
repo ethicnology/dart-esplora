49b8e1697bb4842582e8979877e9fdadc9b29daa
ethicnology <ethicnology@pm.me>
2022-03-22 12:12:12 +0100

    fix: Status blockHeight, blockHash, blockTime could be null

--- lib/src/status.dart
+++ lib/src/status.dart
@@ -2,24 +2,32 @@ import 'utils.dart';
 
 class Status {
   late bool confirmed;
-  late int blockHeight;
-  late String blockHash;
-  late int blockTime;
+  late int? blockHeight;
+  late String? blockHash;
+  late int? blockTime;
 
   Status(this.confirmed, this.blockHeight, this.blockHash, this.blockTime) {
-    isPositive(blockHeight);
-    isEqual(blockHash.length, 64);
-    isPositive(blockTime);
+    if (confirmed == false) {
+      blockHeight = null;
+      blockHash = null;
+      blockTime = null;
+    }
   }
 
   Status.fromJson(Map<String, dynamic> json) {
     confirmed = json['confirmed'];
-    blockHeight = json['block_height'];
-    blockHash = json['block_hash'];
-    blockTime = json['block_time'];
-    isPositive(blockHeight);
-    isEqual(blockHash.length, 64);
-    isPositive(blockTime);
+    if (confirmed == false) {
+      blockHeight = null;
+      blockHash = null;
+      blockTime = null;
+    } else {
+      blockHeight = json['block_height'];
+      blockHash = json['block_hash'];
+      blockTime = json['block_time'];
+      isPositive(blockHeight!);
+      isEqual(blockHash!.length, 64);
+      isPositive(blockTime!);
+    }
   }
 
   Map<String, dynamic> toJson() => {
