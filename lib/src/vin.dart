b033fc874573a9071ef6192fc9996abedd97016b
ethicnology <ethicnology@pm.me>
2022-03-22 10:25:04 +0100

    fix: add nullable prevout and witness for some old bitcoin transactions

--- lib/src/vin.dart
+++ lib/src/vin.dart
@@ -4,10 +4,14 @@ import 'vout.dart';
 class Vin {
   late String txid;
   late int vout;
-  late Vout prevout;
+
+  /// Some old bitcoin txs doesn't have a prevout: 4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b
+  late Vout? prevout;
   late String scriptsig;
   late String scriptsigAsm;
-  late List<String> witness;
+
+  /// Some old bitcoin txs doesn't have a witness: 4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b
+  late List<String>? witness;
   late bool isCoinbase;
   late int sequence;
 
@@ -24,7 +28,7 @@ class Vin {
     prevout = Vout.fromJson(json['prevout']);
     scriptsig = json['scriptsig'];
     scriptsigAsm = json['scriptsig_asm'];
-    witness = List.from(json['witness']);
+    json['witness'] == null ? witness = null : List.from(json['witness']);
     isCoinbase = json['is_coinbase'];
     sequence = json['sequence'];
     isEqual(txid.length, 64);
@@ -35,7 +39,7 @@ class Vin {
   Map<String, dynamic> toJson() => {
         'txid': txid,
         'vout': vout,
-        'prevout': prevout.toJson(),
+        'prevout': prevout?.toJson(),
         'scriptsig': scriptsig,
         'scriptsig_asm': scriptsigAsm,
         'witness': witness,
