08651a30c4c754b9717a8ec2509bc8110b6fa3f2
ethicnology <ethicnology@pm.me>
2022-03-22 15:23:34 +0100

    feat: MerkleProof class with json serialization/deserialization

---
+++ lib/src/merkle_proof.dart
@@ -0,0 +1,29 @@
+import 'utils.dart';
+
+class MerkleProof {
+  late int blockHeight;
+  late List<String> merkle;
+  late int pos;
+
+  MerkleProof(this.blockHeight, this.merkle, this.pos) {
+    isPositive(blockHeight);
+    isPositive(pos);
+    for (var hash in merkle) {
+      isEqual(hash.length, 64);
+    }
+  }
+
+  MerkleProof.fromJson(Map<String, dynamic> json) {
+    blockHeight = json['block_height'];
+    merkle = List.from(json['merkle']);
+    pos = json['pos'];
+    isPositive(blockHeight);
+    isPositive(pos);
+    for (var hash in merkle) {
+      isEqual(hash.length, 64);
+    }
+  }
+
+  Map<String, dynamic> toJson() =>
+      {'block_height': blockHeight, 'merkle': merkle, 'pos': pos};
+}
