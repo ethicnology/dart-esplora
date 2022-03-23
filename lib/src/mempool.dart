493d75211404f4dfe87fd8bcd67da0c2b65845dc
ethicnology <ethicnology@pm.me>
2022-03-23 15:36:50 +0100

    docs: comments Mempool fields

--- lib/src/mempool.dart
+++ lib/src/mempool.dart
@@ -1,9 +1,17 @@
 import 'utils.dart';
 
 class Mempool {
+  /// the number of transactions in the mempool
   late int count;
+
+  /// the total size of mempool transactions in virtual bytes
   late int vsize;
+
+  /// the total fee paid by mempool transactions in satoshis
   late int totalFee;
+
+  /// mempool fee-rate distribution histogram
+  /// An array of (feerate, vsize) tuples, where each entry's vsize is the total vsize of transactions paying more than feerate but less than the previous entry's feerate (except for the first entry, which has no upper bound). This matches the format used by the Electrum RPC protocol for mempool.get_fee_histogram.
   late List<List<num>> feeHistogram;
 
   Mempool(this.count, this.vsize, this.totalFee, this.feeHistogram) {
